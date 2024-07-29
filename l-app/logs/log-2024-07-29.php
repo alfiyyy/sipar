c04467dc7f9627b2d5a5deeb31bdf1a2f5ec6854
alfiyyy <ianalfianyusuf@gmail.com>
2024-07-29 13:53:40 +0700

    penyesuaian list ruangan di pemesanan

---
+++ l-app/logs/log-2024-07-29.php
@@ -0,0 +1,107 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-07-29 13:29:40 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 84
+ERROR - 2024-07-29 13:29:40 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 85
+ERROR - 2024-07-29 13:29:40 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 86
+ERROR - 2024-07-29 13:29:40 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 87
+ERROR - 2024-07-29 13:29:40 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 88
+ERROR - 2024-07-29 13:29:40 --> Severity: Notice --> Undefined variable: unker C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 89
+ERROR - 2024-07-29 13:31:35 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 84
+ERROR - 2024-07-29 13:31:35 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 85
+ERROR - 2024-07-29 13:31:35 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 86
+ERROR - 2024-07-29 13:31:35 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 87
+ERROR - 2024-07-29 13:31:35 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 88
+ERROR - 2024-07-29 13:31:35 --> Severity: Notice --> Undefined variable: unker C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 89
+ERROR - 2024-07-29 13:31:37 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Booking_smr.php 54
+ERROR - 2024-07-29 13:31:37 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near '' at line 2 - Invalid query: SELECT * FROM t_smr
+			WHERE unker_id=
+ERROR - 2024-07-29 13:31:41 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 84
+ERROR - 2024-07-29 13:31:41 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 85
+ERROR - 2024-07-29 13:31:41 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 86
+ERROR - 2024-07-29 13:31:41 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 87
+ERROR - 2024-07-29 13:31:41 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 88
+ERROR - 2024-07-29 13:31:41 --> Severity: Notice --> Undefined variable: unker C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 89
+ERROR - 2024-07-29 13:31:55 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Booking_smr.php 54
+ERROR - 2024-07-29 13:31:55 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near '' at line 2 - Invalid query: SELECT * FROM t_smr
+			WHERE unker_id=
+ERROR - 2024-07-29 13:32:11 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Booking_smr.php 54
+ERROR - 2024-07-29 13:32:11 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near '' at line 2 - Invalid query: SELECT * FROM t_smr
+			WHERE unker_id=
+ERROR - 2024-07-29 13:35:35 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Booking_smr.php 54
+ERROR - 2024-07-29 13:35:35 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near '' at line 2 - Invalid query: SELECT * FROM t_smr
+			WHERE unker_id=
+ERROR - 2024-07-29 13:49:25 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\models\mod\Pesanan_kamar_model.php 52
+ERROR - 2024-07-29 13:49:25 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\models\mod\Pesanan_kamar_model.php 44
+ERROR - 2024-07-29 13:49:28 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\models\mod\Pesanan_smr_model.php 52
+ERROR - 2024-07-29 13:49:28 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\models\mod\Pesanan_smr_model.php 44
+ERROR - 2024-07-29 13:49:32 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\models\mod\Pesanan_csr_model.php 52
+ERROR - 2024-07-29 13:49:32 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\models\mod\Pesanan_csr_model.php 44
+ERROR - 2024-07-29 13:50:58 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 84
+ERROR - 2024-07-29 13:50:58 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 85
+ERROR - 2024-07-29 13:50:58 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 86
+ERROR - 2024-07-29 13:50:58 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 87
+ERROR - 2024-07-29 13:50:58 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 88
+ERROR - 2024-07-29 13:50:58 --> Severity: Notice --> Undefined variable: unker C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 89
+ERROR - 2024-07-29 13:51:08 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Booking_smr.php 54
+ERROR - 2024-07-29 13:51:08 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near '' at line 2 - Invalid query: SELECT * FROM t_smr
+			WHERE unker_id=
+ERROR - 2024-07-29 13:51:10 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 84
+ERROR - 2024-07-29 13:51:10 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 85
+ERROR - 2024-07-29 13:51:10 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 86
+ERROR - 2024-07-29 13:51:10 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 87
+ERROR - 2024-07-29 13:51:10 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 88
+ERROR - 2024-07-29 13:51:10 --> Severity: Notice --> Undefined variable: unker C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 89
+ERROR - 2024-07-29 13:51:21 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Booking_smr.php 54
+ERROR - 2024-07-29 13:51:21 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near '' at line 2 - Invalid query: SELECT * FROM t_smr
+			WHERE unker_id=
+ERROR - 2024-07-29 13:51:24 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 84
+ERROR - 2024-07-29 13:51:24 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 85
+ERROR - 2024-07-29 13:51:24 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 86
+ERROR - 2024-07-29 13:51:24 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 87
+ERROR - 2024-07-29 13:51:24 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 88
+ERROR - 2024-07-29 13:51:24 --> Severity: Notice --> Undefined variable: unker C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 89
+ERROR - 2024-07-29 13:51:46 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 84
+ERROR - 2024-07-29 13:51:46 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 85
+ERROR - 2024-07-29 13:51:46 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 86
+ERROR - 2024-07-29 13:51:46 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 87
+ERROR - 2024-07-29 13:51:46 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 88
+ERROR - 2024-07-29 13:51:46 --> Severity: Notice --> Undefined variable: unker C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 89
+ERROR - 2024-07-29 13:52:04 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Booking_smr.php 54
+ERROR - 2024-07-29 13:52:04 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near '' at line 2 - Invalid query: SELECT * FROM t_smr
+			WHERE unker_id=
+ERROR - 2024-07-29 13:52:05 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 84
+ERROR - 2024-07-29 13:52:05 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 85
+ERROR - 2024-07-29 13:52:05 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 86
+ERROR - 2024-07-29 13:52:05 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 87
+ERROR - 2024-07-29 13:52:05 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 88
+ERROR - 2024-07-29 13:52:05 --> Severity: Notice --> Undefined variable: unker C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 89
+ERROR - 2024-07-29 13:52:09 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_class_smart.php 84
+ERROR - 2024-07-29 13:52:09 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_class_smart.php 85
+ERROR - 2024-07-29 13:52:09 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_class_smart.php 86
+ERROR - 2024-07-29 13:52:09 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_class_smart.php 87
+ERROR - 2024-07-29 13:52:09 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_class_smart.php 88
+ERROR - 2024-07-29 13:52:09 --> Severity: Notice --> Undefined variable: unker C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_class_smart.php 89
+ERROR - 2024-07-29 13:52:14 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Booking_csr.php 54
+ERROR - 2024-07-29 13:52:14 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near '' at line 2 - Invalid query: SELECT * FROM t_csr
+			WHERE unker_id=
+ERROR - 2024-07-29 13:52:15 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_class_smart.php 84
+ERROR - 2024-07-29 13:52:15 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_class_smart.php 85
+ERROR - 2024-07-29 13:52:15 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_class_smart.php 86
+ERROR - 2024-07-29 13:52:15 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_class_smart.php 87
+ERROR - 2024-07-29 13:52:15 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_class_smart.php 88
+ERROR - 2024-07-29 13:52:15 --> Severity: Notice --> Undefined variable: unker C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_class_smart.php 89
+ERROR - 2024-07-29 13:52:22 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 84
+ERROR - 2024-07-29 13:52:22 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 85
+ERROR - 2024-07-29 13:52:22 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 86
+ERROR - 2024-07-29 13:52:22 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 87
+ERROR - 2024-07-29 13:52:22 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 88
+ERROR - 2024-07-29 13:52:22 --> Severity: Notice --> Undefined variable: unker C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 89
+ERROR - 2024-07-29 13:52:36 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Booking_smr.php 54
+ERROR - 2024-07-29 13:52:36 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near '' at line 2 - Invalid query: SELECT * FROM t_smr
+			WHERE unker_id=
+ERROR - 2024-07-29 13:52:45 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 84
+ERROR - 2024-07-29 13:52:45 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 85
+ERROR - 2024-07-29 13:52:45 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 86
+ERROR - 2024-07-29 13:52:45 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 87
+ERROR - 2024-07-29 13:52:45 --> Severity: Notice --> Undefined variable: id_data C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 88
+ERROR - 2024-07-29 13:52:45 --> Severity: Notice --> Undefined variable: unker C:\xampp7\htdocs\sipar\l-app\controllers\l-admin\Kelola_smart_meeting.php 89
--- l-app/views/mod/pesan-kamar/view_index.php
+++ l-app/views/mod/pesan-kamar/view_index.php
@@ -1,58 +1,87 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-<div class="page-inner">
-	<div class="d-sm-flex align-items-center justify-content-between pd-b-20">
-		<div class="pageheader pd-t-20 pd-b-0">
-			<div class="d-flex justify-content-between">
-				<div class="clearfix">
-					<div class="breadcrumb pd-0 pd-b-10 mg-0">
-						<a href="#" class="breadcrumb-item"><?=lang_line('ui_dashboard');?></a>
-						<a href="#" class="breadcrumb-item">Pesan Prasarana</a>
-					</div>
-					<h4 class="pd-0 mg-0 tx-20">Pesan Prasarana</h4>
-				</div>
-			</div>
-		</div>
-		<div class="mg-t-15">
-			
-		</div>
-	</div>
-	
-	<div class="card">
-		<div class="table-responsive">
-			<div class="card-body">
-                <div class="box-body">
-                    <div class="row">
-                        <?php foreach ($unker as $row) { 
-                        $query = $this->db->query('SELECT * FROM t_room_kategori
-                        WHERE unker_id='.$row['unker_id'].' ORDER BY kategori_nama ASC');
-                        $result = $query->result_array();    
-                        ?>
-                        <div class="col-sm-12">
-                            <div class="card card-body ht-lg-200 mb-4 mt-4">
-                                <div class="media">
-                                    <span class="tx-color-04"><img src="http://localhost/pupr/l-content/themes/backend/images/logo-kecil.png" alt=""></span>
-                                    <div class="media-body mg-l-20">
-                                        <h6 class="mg-b-10 text-uppercase">Prasarana</h6>
-                                        <p class="text-black mg-b-10"><?= $row['unker_nama'] ?></p>
-                                        <?=html_entity_decode($row['unker_alamat']);?>
-  
-                                        <?php foreach ($result as $kat) { ?>
-                                            <button type="button" class="btn btn-lg pd-x-15 btn-<?=$kat['kategori_warna']?> btn-uppercase" onclick="window.location='<?=admin_url($this->mod.'/pesan/'.$kat['kategori_id']);?>'"><i class="fa fa-bed mr-2"></i><?= $kat['kategori_nama'] ?></button>
-                                        <?php } ?>
-                                        <button type="button" class="btn btn-lg pd-x-15 btn-warning btn-uppercase" onclick="window.location='<?=admin_url($this->mod.'/pesan-smr/'.$row['unker_id']);?>'"><i class="fa fa-university mr-2"></i>Smart Meeting Room</button>
-                                        <button type="button" class="btn btn-lg pd-x-15 btn-dark btn-uppercase" onclick="window.location='<?=admin_url($this->mod.'/pesan-csr/'.$row['unker_id']);?>'"><i class="fa fa-graduation-cap mr-2"></i>Class Smart Room</button>
-                            
-                                        
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pesan Prasarana</title>
+    <style>
+        .button-container {
+            display: grid;
+            grid-template-columns: repeat(auto-fill, minmax(200px, 1fr)); /* Flexible column layout */
+            gap: 10px; /* Space between buttons */
+            margin-top: 10px;
+            padding: 10px; /* Add padding to prevent overflow */
+            box-sizing: border-box;
+        }
+        .button-container button {
+            width: 100%;
+            margin-bottom: 10px; /* Bottom margin for each button */
+            box-sizing: border-box;
+        }
+        .media-body {
+            display: flex;
+            flex-direction: column;
+        }
+        .media {
+            align-items: flex-start;
+        }
+        .card-body {
+            overflow: hidden;
+        }
+    </style>
+</head>
+<body>
+    <div class="page-inner">
+        <div class="d-sm-flex align-items-center justify-content-between pd-b-20">
+            <div class="pageheader pd-t-20 pd-b-0">
+                <div class="d-flex justify-content-between">
+                    <div class="clearfix">
+                        <div class="breadcrumb pd-0 pd-b-10 mg-0">
+                            <a href="#" class="breadcrumb-item"><?=lang_line('ui_dashboard');?></a>
+                            <a href="#" class="breadcrumb-item">Pesan Prasarana</a>
+                        </div>
+                        <h4 class="pd-0 mg-0 tx-20">Pesan Prasarana</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="mg-t-15"></div>
+        </div>
+        
+        <div class="card">
+            <div class="table-responsive">
+                <div class="card-body">
+                    <div class="box-body">
+                        <div class="row">
+                            <?php foreach ($unker as $row) { 
+                            $query = $this->db->query('SELECT * FROM t_room_kategori WHERE unker_id='.$row['unker_id'].' ORDER BY kategori_nama ASC');
+                            $result = $query->result_array();    
+                            ?>
+                            <div class="col-sm-12">
+                                <div class="card card-body ht-lg-280 mb-4 mt-4">
+                                    <div class="media">
+                                        <span class="tx-color-04"><img src="http://localhost/pupr/l-content/themes/backend/images/logo-kecil.png" alt=""></span>
+                                        <div class="media-body mg-l-20">
+                                            <h6 class="mg-b-10 text-uppercase">Prasarana</h6>
+                                            <p class="text-black mg-b-10"><?= $row['unker_nama'] ?></p>
+                                            <?=html_entity_decode($row['unker_alamat']);?>
+                                            <div class="button-container">
+                                                <?php foreach ($result as $kat) { ?>
+                                                    <button type="button" class="btn btn-lg pd-x-15 btn-<?=$kat['kategori_warna']?> btn-uppercase" onclick="window.location='<?=admin_url($this->mod.'/pesan/'.$kat['kategori_id']);?>'"><i class="fa fa-bed mr-2"></i><?= $kat['kategori_nama'] ?></button>
+                                                <?php } ?>
+                                                <button type="button" class="btn btn-lg pd-x-15 btn-warning btn-uppercase" onclick="window.location='<?=admin_url($this->mod.'/pesan-smr/'.$row['unker_id']);?>'"><i class="fa fa-university mr-2"></i>Smart Meeting Room</button>
+                                                <button type="button" class="btn btn-lg pd-x-15 btn-dark btn-uppercase" onclick="window.location='<?=admin_url($this->mod.'/pesan-csr/'.$row['unker_id']);?>'"><i class="fa fa-graduation-cap mr-2"></i>Class Smart Room</button>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
+                            <?php } ?>
                         </div>
-
-                        
-                        <?php } ?>
                     </div>
                 </div>
-			</div>
-		</div>
-	</div>
-</div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
